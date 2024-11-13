e60303a058ff9cf54edebc774b4141e4155a5020
Victor Farias <60015667+AHRGO@users.noreply.github.com>
2024-11-13 22:56:20 +0000

    Insere o caralho a 4 dentro do rolê

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+function Home() {
+   return <h1>
+      As vezes eu sinto pesar/ 
+      Sem ter você do meu lado, com certeza eu iria vacilar./
+      Mas você esteve aqui,/
+      todo dia me nutrindo, me ajudando a sentir/
+      Algo mais maravilhoso, mais real/ 
+      Meu bem, você é me ilumina com seu sorriso/
+      O dia ao seu lado é sobrenatural/
+      Quando acordo e olho pra ti, me ilumino/
+      Deitada, és angelical/
+      Acordada, carrega a divindade consigo/
+      Minha queria, bastião da paz final/
+      Amo passar os dias ao seu lado/
+      Fomos feitos um para o outro afinal/
+      </h1>
+}
+
+export default Home;
