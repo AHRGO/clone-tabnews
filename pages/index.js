3f95993afffdd5e1603ddb4b5fdb4e9fc8bce68c
Victor Hugo <victor.030farias@gmail.com>
2025-01-28 23:44:30 -0300

    adiciona novas linhas ao poema

--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,40 @@ function Home() {
       Todo o seu amor e carinho<br />
       Que me impedem de ficar sozinho<br />
       Fomos feitos um para o outro afinal<br />
+      <br /><br />
+      Você é meu pedacinho do céu <br />
+      Me traz paz e deixa cada dia mais feliz <br />
+      Meu amor, meu amorzinho <br />
+      Nunca achei que em mim, <br />
+      Pudesse brotar tanto carinho <br />
+      Mas é, assim você o fez <br />
+      E fico cada dia mais admirado  <br />
+      Com o quanto você me faz sorrir <br />
+      E cada hora ao seu lado <br />
+      Me faz querer sempre evoluir <br />
+      Para estar com você <br />
+      Pleno e paciente <br />
+      Para poder te socorrer <br />
+      Quando você desfalecer inconsciente <br />
+      Que baita susto que você meu deu ein menina? <br />
+      Quase achei que ia ter que ir na missa <br />
+      Brigar com Deus por levar minha anjinha <br />
+      Mas tá tudo bem <br />
+      Ele te ama e quer te ver feliz <br />
+      Por isso me botou na sua vida, assim, do além <br />
+      Pra eu te lembrar que comer é uma força motriz <br />
+      E que você precisa continuar sempre firme e forte <br />
+      Porque seu pequeno não consegue lidar <br />
+      Com o mundo gigante a frente <br />
+      Sem parar de pensar <br />
+      No seu amor inclemente <br />
+      Pela minha grandinha perdida <br />
+      Uma criança que insiste em ficar sem comida <br />
+      Mas tá tudo bem <br />
+      Eu te cuido <br />
+      Procuro tudo pra poder <br />
+      Teu bem estar satisfazer <br />
+      E sua felicidade comprazer. <br />
       </h1>
 }
 
