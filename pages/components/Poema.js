bf263f130425715ba46b4c2b19c36954558d2f52
Victor Farias <60015667+AHRGO@users.noreply.github.com>
2025-09-10 09:16:47 +0000

    Componentiza o poema

---
+++ pages/components/Poema.js
@@ -0,0 +1,86 @@
+export default function Poema() {
+    return (
+    <h1>
+      As vezes eu sinto pesar
+      <br />
+      Por não conseguir admitir que,
+      <br />
+      Sem ter você do meu lado, com certeza eu iria vacilar.
+      <br />
+      <br />
+      <br />
+      Mas você esteve aqui,
+      <br />
+      todo dia me nutrindo, me ajudando a sentir
+      <br />
+      Que eu sou especial sim
+      <br />
+      <br />
+      <br />
+      E então você se levantou e me ensinou a sorrir
+      <br />
+      E eu percebi como é belo e sereno
+      <br />
+      Estar do seu lado, sempre pleno
+      <br />
+      E no meio da tempestade, não desistir
+      <br />
+      <br />
+      <br />
+      Aiai meu bem, como me sinto aterreno
+      <br />
+      Vendo seu olhar angelical
+      <br />
+      Sentindo seu amor sobrenatural
+      <br />
+      Vejo cada dia mais, pelo o quê estou vivendo
+      <br />
+      <br />
+      <br />
+      Sua energia me dá paz surreal
+      <br />
+      Todo o seu amor e carinho
+      <br />
+      Que me impedem de ficar sozinho
+      <br />
+      Fomos feitos um para o outro afinal
+      <br />
+      <br />
+      <br />
+      Você é meu pedacinho do céu <br />
+      Me traz paz e deixa cada dia mais feliz <br />
+      Meu amor, meu amorzinho <br />
+      Nunca achei que em mim, <br />
+      Pudesse brotar tanto carinho <br />
+      Mas é, assim você o fez <br />
+      E fico cada dia mais admirado <br />
+      Com o quanto você me faz sorrir <br />
+      E cada hora ao seu lado <br />
+      Me faz querer sempre evoluir <br />
+      Para estar com você <br />
+      Pleno e paciente <br />
+      Para poder te socorrer <br />
+      Quando você desfalecer inconsciente <br />
+      Que baita susto que você meu deu ein menina? <br />
+      Quase achei que ia ter que ir na missa <br />
+      Brigar com Deus por levar minha anjinha <br />
+      Mas tá tudo bem <br />
+      Ele te ama e quer te ver feliz <br />
+      Por isso me botou na sua vida, assim, do além <br />
+      Pra eu te lembrar que comer é uma força motriz <br />
+      E que você precisa continuar sempre firme e forte <br />
+      Porque seu pequeno não consegue lidar <br />
+      Com o mundo gigante a frente <br />
+      Sem parar de pensar <br />
+      No seu amor inclemente <br />
+      Pela minha grandinha perdida <br />
+      Uma criança que insiste em ficar sem comida <br />
+      Mas tá tudo bem <br />
+      Eu te cuido <br />
+      Procuro tudo pra poder <br />
+      Teu bem estar satisfazer <br />
+      E sua felicidade comprazer. <br />
+    </h1>
+  );
+
+}
