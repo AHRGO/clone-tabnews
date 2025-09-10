d7d136becfcefc6e9d524cd243445788fbc03c09
Victor Farias <60015667+AHRGO@users.noreply.github.com>
2025-09-10 09:52:26 +0000

    Muda texto temporariamente

--- pages/components/BannerInicial.js
+++ pages/components/BannerInicial.js
@@ -1,7 +1,7 @@
 export default function BannerInicial() {
     return (
         <section className="flex min-h-screen flex-col items center justify-center bg-gray-900 text-white">
-            <h1 className="text-5xl font-bold mb-6" >Não tem ninguém em casa, volte mais tarde</h1>
+            <h1 className="text-5xl font-bold mb-6" >All Hail The Omnissiah</h1>
             <img
             src="/ad-mech.jpeg"
             alt="Imagem do banner inicial"
