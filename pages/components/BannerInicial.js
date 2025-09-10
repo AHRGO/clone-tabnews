7d77348fddb4ea593808f66dcfda73132956fbba
Victor Farias <60015667+AHRGO@users.noreply.github.com>
2025-09-10 11:00:07 +0000

    jimmin vascaino

--- pages/components/BannerInicial.js
+++ pages/components/BannerInicial.js
@@ -1,9 +1,9 @@
 export default function BannerInicial() {
     return (
         <section className="flex min-h-screen flex-col items center justify-center bg-gray-900 text-white">
-            <h1 className="text-5xl font-bold mb-6" >All Hail The Omnissiah</h1>
+            {/* <h1 className="text-5xl font-bold mb-6" >All Hail The Omnissiah</h1> */}
             <img
-            src="/ad-mech.jpeg"
+            src="/jimmin-vascaino.png"
             alt="Imagem do banner inicial"
             className="w-64 h-auto rounded-lg shadow-lg"
             />
