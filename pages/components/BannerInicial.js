20b85ecbb8058d5ef126e72ec1ee835669126d65
Victor Farias <60015667+AHRGO@users.noreply.github.com>
2025-09-10 09:50:14 +0000

    Adiciona estilos com tailwind e uma imagem ao banner inicial

--- pages/components/BannerInicial.js
+++ pages/components/BannerInicial.js
@@ -1,5 +1,14 @@
 export default function BannerInicial() {
     return (
-        <h1>Não tem ninguém em casa, volte mais tarde</h1>
+        <section className="flex min-h-screen flex-col items center justify-center bg-gray-900 text-white">
+            <h1 className="text-5xl font-bold mb-6" >Não tem ninguém em casa, volte mais tarde</h1>
+            <img
+            src="/ad-mech.jpeg"
+            alt="Imagem do banner inicial"
+            className="w-64 h-auto rounded-lg shadow-lg"
+            />
+            
+        </section>
+        
     );
 }
