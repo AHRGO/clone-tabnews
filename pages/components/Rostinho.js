85c010a53ea645490abbcce1ce9cc052c43852d2
Victor Farias <60015667+AHRGO@users.noreply.github.com>
2025-10-18 15:07:35 +0000

    Cria o rostinho

---
+++ pages/components/Rostinho.js
@@ -0,0 +1,12 @@
+export default function Rostinho() {
+    return(
+        <main className="flex items-center justify-center min-h-screen bg-black">
+            <div className="text-white text-15[vw] leading-none font-mono select-none">
+                <div className="flex flex-col items-center">
+                    <div>{`> <`}</div>
+                    <div className="-mt-[5vw]">͜</div>
+                </div>    
+            </div>
+        </main>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Poema from "./components/Poema";
 import BannerInicial from "./components/BannerInicial";
+import Rostinho from "./components/Rostinho";
 
 function Home() {
   return (
-    <BannerInicial/>
+    <Rostinho />
+    // <BannerInicial/>
     // <Poema/>
 
   );
